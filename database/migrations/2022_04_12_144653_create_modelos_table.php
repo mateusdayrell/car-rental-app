e7dd7b5a47d2fbd613bae23e88f59f891907d7eb
Mateus Fernandes Dayrell <mateus.dayrell@campus.unimontes.int>
2022-04-12 12:57:16 -0300

    first commit

---
+++ database/migrations/2022_04_12_144653_create_modelos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modelos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('brand_id');
+            $table->string('name', 30);
+            $table->string('image', 100);
+            $table->integer('door_qt');
+            $table->integer('seaters');
+            $table->boolean('air_bag');
+            $table->boolean('abs');
+            $table->timestamps();
+    
+            //foreign key (constraints)
+            $table->foreign('brand_id')->references('id')->on('brands');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modelos');
+    }
+}
---
+++ database/migrations/2022_04_12_145154_create_rents_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('car_id');
+            $table->dateTime('bego');
+            $table->dateTime('begin_date');
+            $table->dateTime('end_date');
+            $table->float('daily_value', 8,2);
+            $table->integer('km_start');
+            $table->integer('km_end');
+            $table->timestamps();
+    
+            //foreign key (constraints)
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('car_id')->references('id')->on('cars');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rents');
+    }
+}
